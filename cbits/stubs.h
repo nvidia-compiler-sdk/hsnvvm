8baed6327c826375f880688ae43b04919010fd27
Sean Lee <selee@nvidia.com>
2014-03-31 19:57:00 -0700

    Remove trailing whitespaces.

--- cbits/stubs.h
+++ cbits/stubs.h
@@ -1,7 +1,7 @@
 #ifndef C_STUBS_H
 #define C_STUBS_H
 
-/* 
+/*
  * This part of the code is from https://github.com/tmcdonell/cuda, which
  * is under BSD3 (https://github.com/tmcdonell/cuda/blob/master/LICENSE).
  */
@@ -14,7 +14,7 @@
 #define _ANSI_SOURCE
 #define __AVAILABILITY__
 #define __OSX_AVAILABLE_STARTING(_mac, _iphone)
-#define __OSX_AVAILABLE_BUT_DEPRECATED(_macIntro, _macDep, _iphoneIntro, _iphoneDep) 
+#define __OSX_AVAILABLE_BUT_DEPRECATED(_macIntro, _macDep, _iphoneIntro, _iphoneDep)
 #endif
 
 
